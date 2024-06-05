eabce6972e4c1f338c8a8fc3f29a2a5b96d423dd
erfan <erffon@gmail.com>
2024-06-05 11:29:47 +0330

    initial files and structure with index page design

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+<?php echo get_template_part('partials/nav/nav', 'nav') ?>
+
+<?php echo get_template_part('partials/index/hero-header', 'hero-header') ?>
+
+<?php echo get_template_part('partials/index/trip-facts', 'trip-facts') ?>
+
+<?php echo get_template_part('partials/index/course-slider', 'course-slider') ?>
+
+<?php echo get_template_part('partials/index/post', 'post') ?>
+
+<?php echo get_template_part('partials/index/post-tech', 'post-tech') ?>
+
+<?php echo get_template_part('partials/index/vip', 'vip') ?>
+
+<?php echo get_template_part('partials/index/feedback', 'feedback') ?>
+
+<?php echo get_template_part('partials/index/newsletter', 'newsletter') ?>
+
+<?php get_footer(); ?>
