c53c1fad3abcbc42f3ac4175e9ef9f01635aefa6
erffon <erffon@gmail.com>
2024-06-28 19:05:30 +0330

    Feat: Add closed comments alert in single blog page

--- partials/single/_main-content-blog-content.php
+++ partials/single/_main-content-blog-content.php
@@ -10,35 +10,38 @@
                 </ul>
             </div>
         </div>
-        <div class="comment-box submit-form">
-            <h3 class="reply-title">ثبت دیدگاه</h3>
-            <div class="comment-form">
-                <form action="#">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-12">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="نام کامل">
+        <?php if (comments_open()) : ?>
+            <div class="comment-box submit-form">
+                <h3 class="reply-title">ثبت دیدگاه</h3>
+                <div class="comment-form">
+                    <form action="#">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="نام کامل">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="ایمیل معتبر">
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <div class="form-group">
+                                    <textarea name="comment" class="form-control" cols="30" rows="6" placeholder="نظر خود را بنویسید..."></textarea>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <div class="form-group">
+                                    <a href="#" class="btn search-btn">ثبت</a>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-6 col-sm-12">
-                            <div class="form-group">
-                                <input type="text" class="form-control" placeholder="ایمیل معتبر">
-                            </div>
-                        </div>
-                        <div class="col-lg-12 col-md-12 col-sm-12">
-                            <div class="form-group">
-                                <textarea name="comment" class="form-control" cols="30" rows="6" placeholder="نظر خود را بنویسید..."></textarea>
-                            </div>
-                        </div>
-                        <div class="col-lg-12 col-md-12 col-sm-12">
-                            <div class="form-group">
-                                <a href="#" class="btn search-btn">ثبت</a>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
+                    </form>
+                </div>
+            </div> <?php else : ?>
+            <div class="alert alert-info">دیدگاه‌های این پست بسته است!</div>
+        <?php endif; ?>
     </div>
 
 </div>
