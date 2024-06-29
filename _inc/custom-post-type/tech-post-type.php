34b570e2d3278c9d2adca10972600f69f4372ab4
erfan <erffon@gmail.com>
2024-06-29 16:22:01 +0330

    Feat: Add tech post type and taxonomy init

---
+++ _inc/custom-post-type/tech-post-type.php
@@ -0,0 +1,83 @@
+<?php
+
+function register_tech_post_type()
+{
+    $labels = array(
+        'name'                  => 'تکنولوژی‌ها',
+        'singular_name'         => 'tech',
+        'menu_name'             => 'اخبار تکنولوژی',
+        'name_admin_bar'        => 'tech',
+        'add_new'               => 'افزودن جدید',
+        'add_new_item'          => 'اضافه کردن مطلب تکنولوژی جدید',
+        'new_item'              => 'آیتم جدید',
+        'edit_item'             => 'ویرایش آِتم',
+        'view_item'             => 'مشاهده',
+        'all_items'             => 'همه',
+        'search_items'          => 'جستجو',
+        'parent_item_colon'     => 'آیتم والد',
+        'not_found'             => 'چیزی یافت نشد',
+        'not_found_in_trash'    => 'چیزی در زباله‌دان یافت نشد',
+        'featured_image'        => 'تصویر شاخص',
+        'set_featured_image'    => 'انتخاب بعنوان تصویر شاخص',
+        'remove_featured_image' => 'حذف تصویر شاخص',
+        'use_featured_image'    => 'استفاده از تصویر شاخص',
+        'archives'              => 'آرشیوها',
+        'insert_into_item'      => 'ورود آیتم جدید',
+        'uploaded_to_this_item' => 'آپلود به این آیتم',
+        'filter_items_list'     => 'فیلتر لیست آیتم‌ها',
+        'items_list_navigation' => 'پیمایش بین آیتم‌ها',
+        'items_list'            => 'لیست آیتم‌ها',
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array('slug' => 'tech'),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+    );
+
+    register_post_type('tech', $args);
+}
+function register_custom_taxonomy()
+{
+
+    $labels = array(
+        'name'              => 'دسته بندی‌ها',
+        'singular_name'     => '',
+        'search_items'      => '',
+        'all_items'         => '',
+        'view_item'         => '',
+        'parent_item'       => '',
+        'parent_item_colon' => '',
+        'edit_item'         => '',
+        'update_item'       => '',
+        'add_new_item'      => '',
+        'new_item_name'     => '',
+        'not_found'         => '',
+        'back_to_items'     => '',
+        'menu_name'         => ''
+    );
+    $args = array(
+        'labels'            => $labels,
+        'hierarchical'      => true,
+        'public'            => true,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array('slug' => 'tags'),
+        'show_in_rest'      => true,
+    );
+
+
+    register_taxonomy('tags', 'tech', $args);
+}
+
+add_action('init', 'register_custom_taxonomy');
--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ include_once '_inc/assets/register_assets.php';
 include_once '_inc/theme-setup/theme-setup.php';
 
 // ADD POST LEVEL META BOX
-include_once '_inc/meta-box/meta-box.php';
+// include_once '_inc/meta-box/meta-box.php';
 
 // COUNT POST VIEWS
 include_once 'class/helper-class/PostView.php';
@@ -23,4 +23,4 @@ include_once '_inc/widget/CatsWidget.php';
 include_once '_inc/admin/shortcodes.php';
 
 // ADD CUSTOM POST TYPE TAXONOMY
-include_once '_inc/custom-taxonomy/tech-taxonomy.php';
+include_once '_inc/custom-post-type/tech-post-type.php';
